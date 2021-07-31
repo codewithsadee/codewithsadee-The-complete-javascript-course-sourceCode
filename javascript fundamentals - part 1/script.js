0bad397f912f958981b5ba3906dafe8ca86cd019
codewithsadee <mohammadsadee24@gmail.com>
2021-08-01 04:56:03 +0600

    fandamentals part 1

---
+++ javascript fundamentals - part 1/script.js
@@ -0,0 +1,17 @@
+//LECTURE: values and variebles
+let country = 'Bangladesh';
+let continent = 'asia';
+let population = 16100000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//LECTURE: data types
+let isIsland = false;
+let langueage;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof langueage);
