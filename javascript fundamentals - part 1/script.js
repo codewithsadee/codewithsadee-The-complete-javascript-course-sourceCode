7de1d53adde1c89314c595b6c59623097c28ccda
codewithsadee <mohammadsadee24@gmail.com>
2021-08-02 15:54:57 +0600

    lecture: const, let, var & basic operator

--- javascript fundamentals - part 1/script.js
+++ javascript fundamentals - part 1/script.js
@@ -1,17 +1,39 @@
 //LECTURE: values and variebles
-let country = 'Bangladesh';
-let continent = 'asia';
-let population = 16100000;
+// let country = 'Bangladesh';
+// let continent = 'Asia';
+// let population = 16100000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 //LECTURE: data types
-let isIsland = false;
-let langueage;
+// let isIsland = false;
+// let langueage;
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof langueage);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof langueage);
+
+//LECTURE: let, const, var
+const country = 'Bangladesh';
+const continent = 'Asia';
+let population = 16100000;
+const language = "Bangla";
+
+//LECTURE: bacsic operator
+const halfPopulation = population / 2;
+console.log(population + 1);
+
+//Finland has a population of 6 million. Does your country have more people than Finland?
+const findlandPopulation = 600000;
+console.log(population > findlandPopulation);
+
+//The average population of a country is 33 million people. Does your country have less people than the average country? 
+const avarageCountryPopulation = 3300000;
+console.log(population > avarageCountryPopulation);
+
+// create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, 
+//and its 11 million people speak portuguese' 
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
