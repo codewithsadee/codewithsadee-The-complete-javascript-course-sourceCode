7d640e5f36f9a07dba6646011133d533008c1fd9
codewithsadee <mohammadsadee24@gmail.com>
2021-08-02 20:16:11 +0600

    add: challange folder

---
+++ Challenges/script.js
@@ -0,0 +1,12 @@
+// conding challenge #1
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(markBMI, johnBMI);
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
