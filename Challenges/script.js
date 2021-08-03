e7133d29a21c960949e0dd1bf4903ba27980c432
codewithsadee <mohammadsadee24@gmail.com>
2021-08-04 03:20:03 +0600

    challenge #2 completed

--- Challenges/script.js
+++ Challenges/script.js
@@ -1,4 +1,7 @@
-// conding challenge #1
+/*
+//------------------------- conding challenge #1-------------------------------
+// ----------------------------------------------------------------------------
+
 const markMass = 78;
 const markHeight = 1.69;
 const johnMass = 92;
@@ -10,3 +13,28 @@ console.log(markBMI, johnBMI);
 
 const markHigherBMI = markBMI > johnBMI;
 console.log(markHigherBMI);
+
+// ----------------------------------------------------------------------------
+
+*/
+
+//------------------------- conding challenge #2-------------------------------
+// ----------------------------------------------------------------------------
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+const isMarkHigherBMI = markBMI > johnBMI;
+
+if (isMarkHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+}
+
+// ----------------------------------------------------------------------------
