27bcd88cc509fdd237c786bb6296f5f0886b8fd5
codewithsadee <mohammadsadee24@gmail.com>
2021-08-04 02:59:31 +0600

    lecture: string and template literals

--- javascript fundamentals - part 1/script.js
+++ javascript fundamentals - part 1/script.js
@@ -1,28 +1,43 @@
-//LECTURE: values and variebles
-// let country = 'Bangladesh';
-// let continent = 'Asia';
-// let population = 16100000;
+/*
+//---------------------LECTURE: values and variebles---------------------------
+// ----------------------------------------------------------------------------
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+let country = 'Bangladesh';
+let continent = 'Asia';
+let population = 16100000;
 
-//LECTURE: data types
-// let isIsland = false;
-// let langueage;
+console.log(country);
+console.log(continent);
+console.log(population);
+// ----------------------------------------------------------------------------
 
-// console.log(typeof isIsland);
-// console.log(typeof population);
-// console.log(typeof country);
-// console.log(typeof langueage);
 
-//LECTURE: let, const, var
+//---------------------------LECTURE: data types-------------------------------
+// ----------------------------------------------------------------------------
+
+let isIsland = false;
+let langueage;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof langueage);
+// ----------------------------------------------------------------------------
+
+
+//--------------------------LECTURE: let, const, var---------------------------
+// ----------------------------------------------------------------------------
+
 const country = 'Bangladesh';
 const continent = 'Asia';
 let population = 16100000;
 const language = "Bangla";
+// ----------------------------------------------------------------------------
 
-//LECTURE: bacsic operator
+
+//-----------------------LECTURE: bacsic operator------------------------------
+// ----------------------------------------------------------------------------
+
 const halfPopulation = population / 2;
 console.log(population + 1);
 
@@ -30,10 +45,18 @@ console.log(population + 1);
 const findlandPopulation = 600000;
 console.log(population > findlandPopulation);
 
-//The average population of a country is 33 million people. Does your country have less people than the average country? 
+//The average population of a country is 33 million people. Does your country have less people than the average country?
 const avarageCountryPopulation = 3300000;
 console.log(population > avarageCountryPopulation);
 
-// create a new variable 'description' which contains a string with this format: 'Portugal is in Europe, 
-//and its 11 million people speak portuguese' 
+// create a new variable 'description' which contains a string with this format: 'Portugal is in Europe,
+//and its 11 million people speak portuguese'
 const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+// ----------------------------------------------------------------------------
+*/
+
+// -----------------Lecture: string and template literals----------------------
+// ----------------------------------------------------------------------------
+const description = `Purtogal is in Europe, and its 11 million people speak portuguese`;
+// ----------------------------------------------------------------------------
+
