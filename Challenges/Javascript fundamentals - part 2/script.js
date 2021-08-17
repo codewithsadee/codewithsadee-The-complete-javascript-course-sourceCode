c0fe8f1da4b8025d6400f8d8c7125848b347224e
codewithsadee <mohammadsadee24@gmail.com>
2021-08-17 06:39:57 +0600

    chellange #1

---
+++ Challenges/Javascript fundamentals - part 2/script.js
@@ -0,0 +1,45 @@
+'use strict'
+//---------------------------------Challage #1---------------------------------
+//-----------------------------------------------------------------------------
+/*
+const calcAverage = (a, b, c) =>  (a + b + c) / 3;
+//test data 1
+const dolphinsScore1 = calcAverage(44, 23, 71);
+const koalasScore1 = calcAverage(65, 54, 49);
+
+//test data 2
+const dolphinsScore2 = calcAverage(85, 54, 41);
+const koalasScore2 = calcAverage(23, 34, 27);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if ( avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  } else if ( avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log(`No Winner....`);
+  }
+}
+
+checkWinner(dolphinsScore1, koalasScore1);
+checkWinner(dolphinsScore2, koalasScore2);
+
+//-----------------------------------------------------------------------------
+*/
+
+//---------------------------------Challage #2---------------------------------
+//-----------------------------------------------------------------------------
+
+const calcTip = function(bill) {
+  return bill * (bill >=50 &&  bill <= 300 ? 15: 20) / 100;
+}
+
+const bills = [125, 555, 44];
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length -1])];
+console.log(tips);
+
+const total = [bills[0]+tips[0], bills[1]+tips[1],bills[2]+tips[2]];
+console.log(total);
+
+//-----------------------------------------------------------------------------
--- Class practice project/String literals/app.js
+++ Class practice project/String literals/app.js
@@ -1,28 +1,12 @@
-const hello = `hello world!`;
-console.log(`hi from ${hello.toUpperCase()}`);
+`use strict`
 
-document.write(`
-  <div class="container">
-    <button> click me </button>
-    <div class="content"> </div>
-  </div>
-`);
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let x = 0; x < arr.length; x++) {
-  console.log(`Now array is ${arr[x]} Number line`);
+const country = {
+  country: `Bangladesh`,
+  population: 163,
+  neighbour: [`India`, `China`, `Mayanmar`],
+  checkIsland: function(){
+    return this.isIsland = this.neighbour.length ? false: true;
+  } 
 }
 
-const isArrayLength10 = arr.length;
-if (isArrayLength10) {
-  console.log(`array length is ${isArrayLength10}`);
-} else {
-  console.log(`array length is less than ${isArrayLength10}`);
-}
-
-const definedButton = document.querySelector(`button`);
-const contentElem = document.querySelector(`.content`)
-
-definedButton.addEventListener(`click`, function () {
-  contentElem.classList.toggle(`show`);
-});
+console.log(country.checkIsland())
