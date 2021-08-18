8323d77b6d7b63502d020a45e403591e08db1697
codewithsadee <mohammadsadee24@gmail.com>
2021-08-18 20:24:24 +0600

    lecture: the for loop

--- Class practice/app.js
+++ Class practice/app.js
@@ -2,31 +2,27 @@
 // const prompt = require('prompt-sync')();
 
 
-const userInput = document.querySelector('#userInput');
-const submitBtn = document.querySelector('#submit');
-const resultBox = document.querySelector('#result');
+// const userInput = document.querySelector('#userInput');
+// const submitBtn = document.querySelector('#submit');
+// const resultBox = document.querySelector('#result');
 
-let key = '';
+// let key = '';
 
-// Random number machine
-const generateRandom = function () {
-  const machine = Math.ceil(Math.random() * 5);
-  return machine;
-}
+// // Random number machine
+// const start =  () => {
+//   const machine = Math.ceil(Math.random() * 5);
+//   // return machine;
+//   const inputValue = Number(userInput.value);
+//   const chackUserInput =  machine === inputValue ? true : false;
+//   return chackUserInput;
+// }
 
-const chackUserInput = function () {
-  return Number(userInput.value);
-}
+// document.addEventListener('keypress', e => {
+//   key = e.key;
+//   if (key === 'Enter') {
+//     const showResult = start();
+//     resultBox.innerText = `${String(showResult).toUpperCase()} ${showResult ? '💪' : '👿'}`;
+//     resultBox.style.background = showResult ? '#0dc426' : '#c40d0d';
+//   }
+// });
 
-const isUserTrue = (random, input) => {
-  return random === input ? true : false;
-}
-
-document.addEventListener('keypress', e => {
-  key = e.key;
-  if (key === 'Enter') {
-    const showResult = isUserTrue(generateRandom(), chackUserInput());
-    resultBox.innerText = `${String(showResult).toUpperCase()} ${showResult ? '💪' : '👿'}`;
-    resultBox.style.background = showResult ? '#0dc426' : '#c40d0d';
-  }
-});
